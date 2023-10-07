e1276c34010efd05384a551e14bc357139d59537
suguptayash8 <suguptayash8@gmail.com>
2023-10-07 18:44:33 +0530

    Update ProtypeByFunction.js

--- src/prototype/ProtypeByFunction.js
+++ src/prototype/ProtypeByFunction.js
@@ -1,4 +1,19 @@
-/**
+
+/** Must read how new keyworkd work with function and classes in java script
+ * 
+ * When you use new, it creates a new empty object {}.
+
+It sets the this keyword to point to the newly created object.
+
+It calls the constructor function or the class constructor with the provided arguments. The constructor function or constructor method initializes the object by setting its properties based on the arguments passed in.
+
+Finally, the new operator returns the newly created and initialized object.
+
+It's important to note that using the new operator is typically used when working with constructor functions or classes. If you attempt to use it with a regular function that is not designed to be a constructor, it may not behave as expected, and you might encounter errors.
+*/
+
+/** Must read
+ * Prototypes are used to define shared properties and methods that can be inherited by multiple objects.
  * prototype for function
  * @param {} name 
  * @param {*} age 
@@ -35,16 +50,3 @@ p.isOldAge();
 
 p2.greet();
 p2.isOldAge();
-
-/** Must read how new keyworkd work with function and classes in java script
- * 
- * When you use new, it creates a new empty object {}.
-
-It sets the this keyword to point to the newly created object.
-
-It calls the constructor function or the class constructor with the provided arguments. The constructor function or constructor method initializes the object by setting its properties based on the arguments passed in.
-
-Finally, the new operator returns the newly created and initialized object.
-
-It's important to note that using the new operator is typically used when working with constructor functions or classes. If you attempt to use it with a regular function that is not designed to be a constructor, it may not behave as expected, and you might encounter errors.
-*/
